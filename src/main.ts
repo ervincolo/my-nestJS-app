9d94907f8920fca31e765c8f3643de783272ec37
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2025-10-05 17:17:46 +0200

    A lot of changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFiler } from './all-exeptions.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const {httpAdapter} = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExceptionsFiler(httpAdapter))
+  
+  app.enableCors()
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
